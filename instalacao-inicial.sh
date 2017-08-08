5b7fe8029cc10dfcdaa86b61d867d796ebc43b9a
Edson Dota <edsondota@gmail.com>
2017-08-08 12:12:56 -0300

    Adiciona arquivo com info para instalação inicial

---
+++ instalacao-inicial.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vim git git-core git-flow
+
+# após copiar a chave ~/.ssh
+
+sudo chmod 700 ~/.ssh
+sudo chmod 600 ~/.ssh/id_rsa
+ssh-add
+
+# necessário para instalar o google chrome
+sudo apt-get install libgconf2-dev
+
+# python pip
+sudo apt-get install python-dev python-pip
+
+
+# mysql
+sudo apt-get install mysql-server mysql-client mysql-workbench
